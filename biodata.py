f3b328ce6fcc4725a40c86049cddc4bc68bea4c1
psyyt <161565757+psyyt@users.noreply.github.com>
2024-02-28 14:49:51 +0700

    Add files via upload

---
+++ biodata.py
@@ -0,0 +1,17 @@
+name = "Moch. Salman Alfarizi"
+age = "19"
+alamat = "nyalindnung, sukabumi"
+kelas = "Ti 23 i"
+hobbi = "Reading book"
+gender = "male"
+major = "Engineringg Informatics"
+quote = "Impossible is just an opinion"
+
+print("my name is", name)
+print("my age is", age)
+print("i live in", alamat)
+print("my hobbies is", hobbi)
+print("my gender is", gender)
+print("my class is", kelas)
+print("my major is", major) 
+print("quote's today", quote)
